d78ad6a691f73a694342b6bf34a932baae97b184
Taichi Uemura <t.uemura00@gmail.com>
2023-09-09 23:33:50 +0900

    add section on topical type theory

--- src/index.rkt
+++ src/index.rkt
@@ -7,6 +7,7 @@
   ]
   #:title @%{Synthetic topos theory}
   (include-part "0000.rkt")
+  (include-part "001T.rkt")
   #:back @list[
     (include-part "term-index.rkt")
     (include-part "notation-index.rkt")

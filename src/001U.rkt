44e8e2ec6a300c9bea87ccef62281d9f47f612d1
Taichi Uemura <t.uemura00@gmail.com>
2023-09-10 23:19:01 +0900

    some text

--- src/001U.rkt
+++ src/001U.rkt
@@ -3,6 +3,13 @@
 @section[
   #:id (current-id)
   #:title @%{Universe levels}
+  @paragraph{
+    We fix a @emph{universe hierarchy}.
+    There can be a lot of choices of
+    universe hierarchy design.
+    The following rules are
+    a minimal requirement.
+  }
   (include-part "001V.rkt")
   (include-part "001W.rkt")
   (include-part "001X.rkt")

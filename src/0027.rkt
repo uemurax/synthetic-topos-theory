b5c4e59a4e80e592179d2d876be7cd3cc2e20395
Taichi Uemura <t.uemura00@gmail.com>
2023-09-14 22:33:48 +0900

    introduce the type theory of spaces

--- src/0027.rkt
+++ src/0027.rkt
@@ -1,6 +1,16 @@
 #lang morg
 
+(require morg/math)
+
+(define T "T")
+
 @section[
   #:id (current-id)
   #:title @%{Type theory of spaces}
+  @paragraph{
+    Given a type theory @(math T),
+    we introduce the
+    @emph{type theory of spaces in @(math T)}.
+    It is a type theory internal to @(math T).
+  }
 ]

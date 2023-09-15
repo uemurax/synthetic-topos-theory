f81ce1f2d5f96de23309cf76cab5bdf325c62136
Taichi Uemura <t.uemura00@gmail.com>
2023-09-15 21:18:31 +0900

    introduce type theory of sheaves

--- src/0028.rkt
+++ src/0028.rkt
@@ -1,6 +1,21 @@
 #lang morg
 
+(require morg/math
+         (prefix-in tex: morg/math/tex))
+
+(define T "T")
+(define S tex:Sigma)
+
 @section[
   #:id (current-id)
   #:title @%{Type theory of sheaves}
+  @paragraph{
+    Let @(math T) be a type theory
+    and let @(math S) be a geometric theory
+    in @(math T).
+    We introduce the
+    @emph{type theory of sheaves on @(math S)}.
+    It is a type theory
+    internal to @(math T).
+  }
 ]

2d466b0530fc0b184c98a0118166e1080dc69eea
Taichi Uemura <t.uemura00@gmail.com>
2023-09-15 21:21:19 +0900

    type theory of sheaves is a base type theory

--- src/002G.rkt
+++ src/002G.rkt
@@ -1,22 +1,19 @@
 #lang morg
 
-(require morg/math
+(require "lib/article/rule.rkt"
+         morg/math
          (prefix-in tex: morg/math/tex))
 
 (define T "T")
 (define S tex:Sigma)
 
-@section[
+@rule[
   #:id (current-id)
-  #:title @%{Type theory of sheaves}
   @paragraph{
     Let @(math T) be a type theory
     and let @(math S) be a geometric theory
     in @(math T).
-    We introduce the
-    @emph{type theory of sheaves on @(math S)}.
-    It is a type theory
-    internal to @(math T).
+    Then the type theory of sheaves on @(math S)
+    is a base type theory.
   }
-  (include-part "002G.rkt")
 ]

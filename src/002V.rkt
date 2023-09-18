35d9186cf7cce330278c778c918a60c6e7aaeb04
Taichi Uemura <t.uemura00@gmail.com>
2023-09-18 15:16:37 +0900

    some text

--- src/002V.rkt
+++ src/002V.rkt
@@ -1,9 +1,40 @@
 #lang morg
 
+(require morg/math
+         (prefix-in tex: morg/math/tex)
+         "lib/math/type-theory/sheaf.rkt")
+
+(define S tex:Sigma)
+
 @section[
   #:id (current-id)
   #:title @%{Cross-theory axioms}
+  @paragraph{
+    We have introduced
+    the type theory of spaces
+    and the type theory of sheaves
+    both of which are
+    internal to a base type theory.
+    Now we introduce some axioms
+    that strongly tie these type theories together.
+  }
+  @paragraph{
+    The first axiom (@ref["002S"])
+    is between the base type theory
+    and the type theory of spaces.
+    It asserts that types in the base type theory
+    are equivalent to étale spaces.
+  }
   (include-part "002S.rkt")
+  @paragraph{
+    The second axiom (@ref["002Y"])
+    relates type theories of spaces
+    in different base type theories.
+    Let @(math S) be a geometric theory.
+    Then @ref["002Y"] asserts that
+    spaces over @(math S) are equivalent to
+    spaces in @(math (Sheaf S)).
+  }
   (include-part "002W.rkt")
   (include-part "002X.rkt")
   (include-part "002Y.rkt")

a5b35e895b1f60f8cd0603453d435143ae9817c7
Taichi Uemura <t.uemura00@gmail.com>
2023-09-22 08:37:50 +0900

    add section on topos theory

--- src/index.rkt
+++ src/index.rkt
@@ -8,6 +8,7 @@
   #:title @%{Synthetic topos theory}
   (include-part "0000.rkt")
   (include-part "001T.rkt")
+  (include-part "0039.rkt")
   #:back @list[
     (include-part "term-index.rkt")
     (include-part "notation-index.rkt")

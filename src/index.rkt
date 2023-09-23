a085a145cf13a6e485e78cb28b785d9172d4b7ab
Taichi Uemura <t.uemura00@gmail.com>
2023-09-23 20:52:59 +0900

    add section on the topos of types

--- src/index.rkt
+++ src/index.rkt
@@ -9,6 +9,7 @@
   (include-part "0000.rkt")
   (include-part "001T.rkt")
   (include-part "0039.rkt")
+  (include-part "003V.rkt")
   #:back @list[
     (include-part "term-index.rkt")
     (include-part "notation-index.rkt")

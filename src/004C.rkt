fe59a7d1dd74301bca848b489b69008bb5d5a5ae
Taichi Uemura <t.uemura00@gmail.com>
2023-09-25 21:06:28 +0900

    some text

--- src/004C.rkt
+++ src/004C.rkt
@@ -1,6 +1,24 @@
 #lang morg
 
+(require morg/math
+         "lib/math/topos.rkt"
+         "lib/math/function.rkt")
+
+(define X "X")
+
 @section[
   #:id (current-id)
   #:title @%{Internal topos theory}
+  @paragraph{
+    Topos theory is
+    @emph{relative} to a base logos.
+    The topos theory we have presented
+    is the topos theory relative
+    to the category of types.
+    For every topos @(math X),
+    we can also develop the topos theory
+    relative to @(math (Sh . $ . X))
+    by understanding every word
+    internally to @(math (Sh . $ . X)).
+  }
 ]

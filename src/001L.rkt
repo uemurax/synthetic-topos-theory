846601dad73dfd596980df888494df0e5553a696
Taichi Uemura <t.uemura00@gmail.com>
2023-10-01 22:43:19 +0900

    texts on universal property of Topos

--- src/001L.rkt
+++ src/001L.rkt
@@ -8,6 +8,15 @@
 @section[
   #:id (current-id)
   #:title @%{A universal property of @(math (Topos1 U))}
+  @paragraph{
+    The following observation is
+    not directly used elsewhere
+    but indicates that
+    étale morphisms are the characteristic
+    of topos theory
+    and provides inspiration
+    for the design of a type theory of toposes.
+  }
   (include-part "001M.rkt")
   (include-part "001N.rkt")
 ]

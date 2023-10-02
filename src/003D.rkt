faffd1b5b6e60c2061239967f5a060e74667db1a
Taichi Uemura <t.uemura00@gmail.com>
2023-10-03 08:56:31 +0900

    some texts on classifying space

--- src/003D.rkt
+++ src/003D.rkt
@@ -3,8 +3,16 @@
 @section[
   #:id (current-id)
   #:title @%{Classifying spaces}
+  @paragraph{
+    Every space classifies something.
+  }
   (include-part "003E.rkt")
-  (include-part "004R.rkt")
   (include-part "003C.rkt")
   (include-part "003F.rkt")
+  @paragraph{
+    What a space classifies
+    can be calculated by
+    @ref["004R"] below.
+  }
+  (include-part "004R.rkt")
 ]

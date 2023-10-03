ff26dbfee89204e25556e3c07692825869e30976
Taichi Uemura <t.uemura00@gmail.com>
2023-10-03 22:43:52 +0900

    some texts on the topos of propositions

--- src/004M.rkt
+++ src/004M.rkt
@@ -4,6 +4,14 @@
   #:id (current-id)
   #:title @%{The topos of propositions}
   (include-part "004N.rkt")
+  @paragraph{
+    In classical topos theory,
+    the topos of propositions
+    is also called the @emph{Sierpinski topos},
+    and its category of sheaves is
+    the arrow category of
+    the category of types.
+  }
   (include-part "004O.rkt")
   (include-part "004P.rkt")
   (include-part "004Q.rkt")

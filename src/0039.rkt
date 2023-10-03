ec8156dc5b4bb930c2b952d5ca85f590cc9908a8
Taichi Uemura <t.uemura00@gmail.com>
2023-10-03 21:52:05 +0900

    some texts on topos theory

--- src/0039.rkt
+++ src/0039.rkt
@@ -3,6 +3,10 @@
 @section[
   #:id (current-id)
   #:title @%{Topos theory}
+  @paragraph{
+    We develop basic topos theory
+    in the topical type theory.
+  }
   #:subsections @list[
     (include-part "003D.rkt")
     (include-part "003A.rkt")

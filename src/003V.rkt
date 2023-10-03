c43af1b5c2eab195bc86ca053a5b2bb84bd6ba76
Taichi Uemura <t.uemura00@gmail.com>
2023-10-03 22:32:37 +0900

    some texts on the topos of types

--- src/003V.rkt
+++ src/003V.rkt
@@ -3,7 +3,27 @@
 @section[
   #:id (current-id)
   #:title @%{The topos of types}
+  @paragraph{
+    We take a close look at
+    the simplest and most important topos,
+    the @emph{topos of types}.
+  }
   (include-part "003W.rkt")
+  @paragraph{
+    In classical topos theory,
+    the topos of types is also called
+    the @emph{object classifier},
+    and its category of sheaves
+    is the category of presheaves
+    on the free lex category over one object.
+    In particular,
+    every object in the
+    free lex category over one object
+    induces an essential point
+    of the topos of types.
+    We construct those essential points
+    @emph{synthetically}.
+  }
   (include-part "003X.rkt")
   (include-part "003Y.rkt")
   (include-part "003Z.rkt")

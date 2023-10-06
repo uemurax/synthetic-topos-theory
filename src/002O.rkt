5187800e1b7f3cad1e5d1de8040ccf4eb53102a6
Taichi Uemura <t.uemura00@gmail.com>
2023-10-06 22:42:05 +0900

    remove outdated sentence

--- src/002O.rkt
+++ src/002O.rkt
@@ -2,16 +2,8 @@
 
 (require morg/math)
 
-(define T "T")
-
 @section[
   #:id (current-id)
   #:title @%{Toposes}
-  @paragraph{
-    Let @(math T) be a type theory.
-    The type theory of spaces in @(math T)
-    also acts as
-    a @emph{logical framework for toposes}.
-  }
   (include-part "002E.rkt")
 ]

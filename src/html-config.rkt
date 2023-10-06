698cc7f0ab64816190cd4aec3ef741ec764d6bb4
Taichi Uemura <t.uemura00@gmail.com>
2023-10-06 22:56:56 +0900

    custom style

--- src/html-config.rkt
+++ src/html-config.rkt
@@ -1,8 +1,29 @@
-#lang typed/racket
+#lang at-exp typed/racket
 
 (require morg/html
+         morg
          (prefix-in eq: morg/eq-reasoning/html))
 
+(define custom-css-name "topos.css")
+
+(define custom-css
+  @%{
+    .@|statement-body-class-name| {
+      background-color: #F4FDFD;
+      padding: 0.5em;
+    }
+    .@|statement-header-class-name| {
+      padding: 0.5em;
+      background-color: #E3FDFD;
+    }
+    .@|proof-class-name| {
+      background-color: #F4F8F8;
+      padding: 0.5em;
+    }
+  })
+
 (provide-config
- (define cfg default-config)
- (eq:config-update cfg))
+ (compose-config
+  eq:config-update
+  (config-add-css custom-css-name custom-css)
+  (config-set-base-url "https://uemurax.github.io/synthetic-topos-theory")))

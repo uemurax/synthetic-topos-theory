81d806ba716dadb776ebfe43466dedef4227d053
Taichi Uemura <t.uemura00@gmail.com>
2023-10-29 10:51:09 +0900

    remark on total/partial order

--- src/0015.rkt
+++ src/0015.rkt
@@ -4,12 +4,14 @@
          "lib/math/action.rkt"
          "lib/math/function.rkt"
          "lib/math/tuple.rkt"
+         "lib/math/topos-2.rkt"
          "lib/math/topos.rkt")
 
 (define U "U")
 (define V "V")
 (define C "C")
 (define X "X")
+(define I "I")
 
 @section[
   #:id (current-id)
@@ -51,8 +53,27 @@
   }
   (include-part "0053.rkt")
   (include-part "0054.rkt")
+  @paragraph{
+    One can also show that
+    @(math interval) is totally ordered
+    in @(math (Topos U)),
+    but that property would not be preserved
+    by the embedding
+    @(math ((Topos U) . -> . (ShTopos U V))).
+  }
   (include-part "0055.rkt")
   (include-part "0032.rkt")
+  @paragraph{
+    The locality for the first two morphisms
+    in @ref["0032"] is the
+    @emph{Segal condition}
+    and the @emph{Rezk condition}, respectively.
+    We add the third morphism
+    because @(math I) is not assumed to be
+    totally ordered.
+    Local objects for the third morphism
+    “believe that @(math I) is totally ordered”.
+  }
   (include-part "005A.rkt")
   (include-part "0034.rkt")
   (include-part "0035.rkt")
